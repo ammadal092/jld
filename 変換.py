e367db98a7a814e56a46c2aa587e536093396a3c
ammadal092 <anyarobert13@gmail.com>
2025-02-23 19:22:25 +0900

    menambah vocab 2

--- 変換.py
+++ 変換.py
@@ -1,7 +1,7 @@
 import pyperclip
-kata1='可動時 引っ張られる 取付後 ねじれ'
-nomor="41"
-kata2="かどうじsaatbisadigerakkan ひっぱられるmenarikmerenggang引っ張る setelahinstalasi berbelit belit memeutar"
+kata1='日本が日本でなくなる？観光公害どう対策？インバウンド目標は無理'
+nomor="44"
+kata2="こうがいpolusi/negatif たいさくtindakanuntukmasalah もくひょうsasaran/tujuan むりmustahil inbound>mengarahkewistawanasing"
 input1 = '<td>'+kata1+'<button id="button'+nomor+'" type="button" class="btn btn-secondary btn-sm">確認</button></td><td id="d'+nomor+'"><span class="d-none">'+kata2+'</span></td>'
 
 
